41ce66d8f4544e5247b200debd1f1abd09805943
mark <mark.miao@inspiry.cn>
2016-10-12 10:35:14 +0800

    fmdboc

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # XDSQLiteOperate
 SQLite数据库的几种操作方式
+
+
+## 使用FMDB访问SQLite数据库OC
+
+## 使用FMDB访问SQLite数据库Swift
+
+## 使用YTKKeyValueStore访问SQLite数据库OC
+
+## 使用YTKKeyValueStore访问SQLite数据库Swift
+
---
+++ fmdboc/fmdboc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  fmdboc
+//
+//  Created by miaoxiaodong on 16/10/12.
+//  Copyright © 2016年 miaoxiaodong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
